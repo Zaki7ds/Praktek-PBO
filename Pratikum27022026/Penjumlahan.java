33cfbf555960190beb50291f2b74edfef3525af3
Zaki7ds <zakiviki01@gmail.com>
2026-07-06 20:53:32 +0700

    Add files via upload

---
+++ Pratikum27022026/Penjumlahan.java
@@ -0,0 +1,23 @@
+package Pratikum27022026;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *  Program :Penjumlahan.Java
+ *  Pembuat :Fitrah azzaki
+ *  Tanggal : 27 Februari 2026
+ *  Deskripsi : Penjumlahn dua buah bilangan bulat 
+ */
+public class Penjumlahan {
+    public static void main(String[]args){
+        int a,b,c;
+        a=10;
+        b=5;
+        c = a + b;
+        System.out.println("a + b = " + c );
+        System.out.println(a + " + " + b + " = " + c );
+    }
+}
